52c7fcba584915adc8f6b35890ddfb3a301b9aff
Nahuel Frias <nahuelfrias@outlook.com.ar>
2025-08-03 13:31:10 -0300

    Agrego iconos y cambio la lista de opciones a fuera del boton

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import { Ellipsis } from "lucide-react";
+import { Ellipsis, SquarePen, Trash } from "lucide-react";
 import FormatTime from "../utils/FormatTime";
 import { useState } from "react";
 
@@ -9,36 +9,52 @@ export default function Post({className, text, username, date, avatar = "https:/
   return (
     <>
       <section className={`${className} flex justify-between p-4`}>
-        <div className="flex gap-x-2 w-full">
+        <div className="flex gap-x-5 w-full">
           <img className="w-10 h-10 bg-red-50 rounded-full flex-shrink-0" src={avatar} alt="Avatar del usuario" />
           <article className="w-full">
-            <div className="flex justify-between items-center">
-              <h2 className="font-bold flex gap-x-2">{username} <span className="font-normal text-gray-400 flex items-center gap-x-2"><div className="w-1 h-1 bg-gray-400 rounded-full"></div> {FormatTime(date)}</span></h2>
-              <button className="cursor-pointer hover:bg-blue-400/20 hover:text-blue-500 rounded-full p-1"  onClick={(e) => {e.stopPropagation()}}>
-                <Ellipsis onClick={() => setOptions(!options)}/>
+           <div className="flex justify-between items-center relative">
+              <h2 className="font-bold flex gap-x-2">
+                {username}
+                <span className="font-normal text-gray-400 flex items-center gap-x-2">
+                  <div className="w-1 h-1 bg-gray-400 rounded-full"></div>
+                  {FormatTime(date)}
+                </span>
+              </h2>
+
+              <div className="relative">
+                <button
+                  className="cursor-pointer hover:bg-blue-400/20 hover:text-blue-500 rounded-full p-1"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setOptions(!options);
+                  }}
+                >
+                  <Ellipsis />
+                </button>
+
                 {options && (
                   <div
-                    className="absolute bg-white shadow-lg rounded-md mt-2"
+                    className="absolute right-0 mt-2 bg-black border border-white/10 text-white shadow-[0_0_16px_rgba(0,0,0,0.85)] z-10 shadow-gray-800/50 rounded-md w-48"
                     onClick={(e) => e.stopPropagation()}
                   >
                     <button
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                      className="flex gap-x-2 hover:bg-gray-300/10 px-4 py-2 text-sm w-full text-left"
                       onClick={() => setOptions(false)}
                     >
-                      Editar
+                      <SquarePen className="w-5 h-5" /> Editar
                     </button>
                     <button
-                      className="block px-4 py-2 text-sm text-red-600 hover:bg-red-100 w-full text-left"
+                      className="flex gap-x-2 hover:bg-gray-300/10 px-4 py-2 text-sm w-full text-left"
                       onClick={() => {
                         setOptions(false);
                         onDelete?.();
                       }}
                     >
-                      Borrar
+                      <Trash className="w-5 h-5" /> Borrar
                     </button>
                   </div>
                 )}
-              </button>
+              </div>
             </div>
             <span>{text}</span>
           </article>
